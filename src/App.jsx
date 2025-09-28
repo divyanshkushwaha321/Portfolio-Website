b65faf5302e45669b32590de85934d70fd402fa8
Divyansh Kushwaha <divyanshkushwaha69@gmail.com>
2025-09-29 00:06:40 +0530

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigation from './components/Navigation';
 import Hero from './components/Hero';
 import About from './components/About';
 import Experience from './components/Experience';
-import Projects from './components/projects';
+import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './App.css';
 
